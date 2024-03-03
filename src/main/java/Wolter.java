7ec5d1702ae688f1c79ebd69abd8bdc8ace83bb3
talgabay7 <126403499+talgabay7@users.noreply.github.com>
2024-03-03 08:44:12 +0200

    Create Wolter.java

---
+++ src/main/java/Wolter.java
@@ -0,0 +1,21 @@
+public class Wolter {
+	private String id;
+	private int cc;
+	public Wolter(String id,int cc) {
+		this.id=id;
+		this.cc=cc;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+	
+}
